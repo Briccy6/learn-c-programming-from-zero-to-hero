a8f6b06023aa0d8843299460044eb7eac19b1b65
Briccy6 <hirwabrian9@gmail.com>
2026-02-03 00:57:43 +0200

    added the codes for the variables with f to tell that it is float not double which is the best approach

---
+++ 01-basics/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 20;
+    float height = 1.75f;
+    char grade = 'A';
+
+    printf("Age: %d years\n", age);
+    printf("Height: %.2f meters\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
